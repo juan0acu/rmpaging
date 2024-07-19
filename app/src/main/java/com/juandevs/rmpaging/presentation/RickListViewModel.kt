eb89d896df598e1608f1d53baa489cdb47292fd8
Juanchodevs <juan0acu@gmail.com>
2024-07-19 17:48:39 -0400

    ViewModel + Screen y Card

--- app/src/main/java/com/juandevs/rmpaging/presentation/RickListViewModel.kt
+++ app/src/main/java/com/juandevs/rmpaging/presentation/RickListViewModel.kt
@@ -1,9 +1,15 @@
 package com.juandevs.rmpaging.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagingData
+import com.juandevs.rmpaging.data.RickMortyRepository
+import com.juandevs.rmpaging.presentation.model.CharacterModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
-class RickListViewModel  @Inject constructor(): ViewModel() {
+class RickListViewModel  @Inject constructor(rickMortyRepository: RickMortyRepository): ViewModel() {
+
+    val characters: Flow<PagingData<CharacterModel>> = rickMortyRepository.getAllCharacter()
 }
